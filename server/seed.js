e804e5568f520ff3e20319d59c06569347d330dc
Jacob Franze <jfranze@andrew.cmu.edu>
2026-04-21 17:25:17 -0400

    delete for redownload

--- server/seed.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs'
-import path from 'path'
-import { loadDb } from './dataStore.js'
-
-const dbPath = path.resolve('server/data/db.json')
-if (fs.existsSync(dbPath)) fs.unlinkSync(dbPath)
-const db = loadDb()
-console.log(`Seeded ${db.shopStockLines.length} shop stock lines, ${db.imports.length} imports.`)
