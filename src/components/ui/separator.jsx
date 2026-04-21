3580d64f607279148761b4ae60876d1076f3dc4b
Jacob Franze <jfranze@andrew.cmu.edu>
2026-04-21 17:17:57 -0400

    Postgres Comptaible

--- src/components/ui/separator.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { cn } from '@/lib/utils'
-
-export function Separator({ className = '', ...props }) {
-  return <div className={cn('h-px w-full', className)} {...props} />
-}
